6419ad80a44b8dbcf2b2d732d2a8e85e86bba6e0
Timo Grossenbacher <wnstnsmth@yahoo.com>
2015-02-09 14:19:00 +0100

    almost finished quiz 1

--- GettingAndCleaningData/Quiz1/quiz.R
+++ GettingAndCleaningData/Quiz1/quiz.R
@@ -1,2 +1,16 @@
 # question 1
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv", "acs.csv", "curl")
+# other stuff
+
+# question 3
+install.packages("rJava")
+install.packages("xlsx")
+
+library(xlsx)
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx", "nga.xlsx", "curl")
+
+install.packages("XML")
+
+library(XML)
+
+
